6cb11bbaec5bb593967240e0e33ed3bf9dd476c7
LIMPIX31 <limpix31@gmail.com>
2022-10-22 10:10:33 +0200

    feat: user input

---
+++ src/calc.rs
@@ -0,0 +1,3 @@
+pub fn calculate(input: &str) -> f64 {
+  0.0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod calc;
+
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut input: String = "".to_string();
+    stdin.read_line(&mut input).expect("Shit happened");
+    println!("You typed {}", input);
 }
